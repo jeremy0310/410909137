9282ee96847ad46dba79fa4457a02e5627c0b6e3
jeremy0310 <145034849+jeremy0310@users.noreply.github.com>
2023-11-13 12:02:01 +0800

    Add files via upload

--- about.py
+++ about.py
@@ -7,29 +7,28 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     x = "<h1>企管四B陳昭毅的求職相關資訊</h1>"
-    x += "<a href=/about>個人簡介</a><br>"
+    x += "<a href=/aboutme>個人簡介</a><br>"
     x += "<a href=/work>ERP系統工作相關介紹</a><br>"
-    x += "<a href=/test>職涯測驗分析</a><br>"
-    x += "<a href=/myself>自傳履歷</a><br>"
+    x += "<a href=/UCAN>職涯測驗分析</a><br>"
+    x += "<a href=/future>自傳履歷</a><br>"
     return x
 
-
-@app.route("/about")
+@app.route("/aboutme")
 def about():
-    return render_template("about.html")
+    return render_template("aboutme.html")
 
 @app.route("/work")
 def work():
     return render_template("work.html")
     
 
-@app.route("/test")
+@app.route("/UCAN")
 def test():
-    return render_template("test.html")
+    return render_template("UCAN.html")
 
-@app.route("/myself")
+@app.route("/future")
 def myself():
-    return render_template("myself.html")
+    return render_template("future.html")
 
 if __name__ == "__main__":
     app.run()
